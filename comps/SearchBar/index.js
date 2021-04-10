977e410922e270f1506674952f3d6df552743ff4
Daniel Silver <dabzbm@gmail.com>
2021-04-10 12:49:25 -0400

    'fixed links'

--- comps/SearchBar/index.js
+++ comps/SearchBar/index.js
@@ -1,9 +1,18 @@
 import styles from './Search.module.css';
+import { FaReact } from 'react-icons/fa';
+import Link from 'next/link';
+
 const SearchBar = ({ ...rest }) => {
   return (
-    <div className={styles.coin_search}>
-      <input className={styles.coin_input} {...rest} />
-    </div>
+    <>
+      <Link href='https://cryptotrack-indol.vercel.app/'>
+        <FaReact size={40} className={styles.icon} />
+      </Link>
+
+      <div className={styles.coin_search}>
+        <input className={styles.coin_input} {...rest} />
+      </div>
+    </>
   );
 };
 
